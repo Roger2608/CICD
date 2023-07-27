91870d34259228e207e7d0a73351d41f6228ec31
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2023-07-27 18:43:53 -0500

    modify job app

--- devops/jenkins/app/job_dsl_app_script.groovy
+++ devops/jenkins/app/job_dsl_app_script.groovy
@@ -1,34 +1,16 @@
-// Definición del Job DSL
-job('JobDSL_GenerateJob') {
-    description('Job DSL para generar un nuevo Job')
-
-    // Define los pasos para clonar el repositorio privado utilizando las credenciales
-    scm {
-        git {
-            remote {
-                url('https://github.com/Roger2608/CICD.git')
-                credentials('JenkinsGithub')
-            }
-        }
-    }
-
-    // Define el script de Job DSL
-    steps {
-        script {
-            def dslScriptPath = 'devops/jenkins/component/job_dsl_component_script.groovy'
-            def generatedDSL
-
-            if (params.NameApp == '') {
-                error('¡Por favor, proporciona un valor para el parámetro NameApp!')
-            }
-
-            // Aquí puedes agregar parámetros o configuraciones adicionales para el DSL, si es necesario
-
-            // Ejecutar el script Job DSL
-            generatedDSL = load(dslScriptPath)
-
-            // Almacenar el resultado del DSL generado para su posterior uso
-            writeFile file: "generated_dsl_${params.NameApp}.groovy", text: generatedDSL
-        }
-    }
+folder("${appName}") {
+  		displayName("${appName}")
+        description("Descripción del Folder para proyecto ${appName}")
+	job("${appName}/job_${appName}_for_components") {
+		description('Job DSL para generar un nuevo Job')
+		// Define los pasos para clonar el repositorio privado utilizando las credenciales
+		scm {
+			git {
+				remote {
+					url('https://github.com/Roger2608/CICD.git')
+					credentials('JenkinsGithub')
+				}
+			}
+		}
+	}
 }
