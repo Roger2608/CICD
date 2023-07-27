b3076a6ea9e73dbf2e9e67d76cec8d78e2974b26
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2023-07-27 17:14:44 -0500

    new script

--- devops/jenkins/app/job_dsl_app_script.groovy
+++ devops/jenkins/app/job_dsl_app_script.groovy
@@ -1,56 +1,34 @@
-pipeline {
-    parameters {
-        string(name: 'NameApp', defaultValue: '', description: 'Nombre de la aplicación para el Job DSL')
+// Definición del Job DSL
+job('JobDSL_GenerateJob') {
+    description('Job DSL para generar un nuevo Job')
+
+    // Define los pasos para clonar el repositorio privado utilizando las credenciales
+    scm {
+        git {
+            remote {
+                url('https://github.com/Roger2608/CICD.git')
+                credentials('JenkinsGithub')
+            }
+        }
     }
-    agent any
-    stages {
-        stage('Clonar Repositorio') {
-            steps {
-                // Utiliza las credenciales de Jenkins para acceder al repositorio privado
-                withCredentials([usernamePassword(credentialsId: 'JenkinsGithub', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
-                    // Clonar el repositorio privado utilizando las credenciales proporcionadas
-                    sh "git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@https://github.com/Roger2608/CICD.git"
-                }
+
+    // Define el script de Job DSL
+    steps {
+        script {
+            def dslScriptPath = 'devops/jenkins/component/job_dsl_component_script.groovy'
+            def generatedDSL
+
+            if (params.NameApp == '') {
+                error('¡Por favor, proporciona un valor para el parámetro NameApp!')
             }
-        }
-        stage('Generar Job DSL') {
-            steps {
-                script {
-                    def dslScriptPath = 'devops/jenkins/component/job_dsl_component_script.groovy'
-                    def generatedDSL
 
-                    // Validar que se haya proporcionado un valor para el parámetro NameApp
-                    if (params.NameApp == '') {
-                        error('¡Por favor, proporciona un valor para el parámetro NameApp!')
-                    }
+            // Aquí puedes agregar parámetros o configuraciones adicionales para el DSL, si es necesario
 
-                    // Aquí puedes agregar parámetros o configuraciones adicionales para el DSL, si es necesario
+            // Ejecutar el script Job DSL
+            generatedDSL = load(dslScriptPath)
 
-                    // Ejecutar el script Job DSL
-                    generatedDSL = load(dslScriptPath)
-
-                    // Almacenar el resultado del DSL generado para su posterior uso
-                    writeFile file: "generated_dsl_${params.NameApp}.groovy", text: generatedDSL
-                }
-            }
-        }
-        stage('Crear Folder y Job DSL') {
-            steps {
-                script {
-                    def folderName = params.NameApp
-                    def generatedDSLPath = "generated_dsl_${folderName}.groovy"
-
-                    // Crear el folder con el nombre proporcionado
-                    folder(folderName) {
-                        // Crear el nuevo job DSL dentro del folder
-                        jobDsl targets: generatedDSLPath
-
-                        // Si tienes más pasos o configuraciones que deseas agregar al Job DSL creado,
-                        // puedes hacerlo aquí utilizando los pasos de Jenkins que desees.
-                    }
-                }
-            }
+            // Almacenar el resultado del DSL generado para su posterior uso
+            writeFile file: "generated_dsl_${params.NameApp}.groovy", text: generatedDSL
         }
     }
 }
-
