859307405912ae8ec591519b78ef09bb0e3141a5
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2023-07-29 16:21:35 -0500

    add branch

--- devops/jenkins/app/job_dsl_app_script.groovy
+++ devops/jenkins/app/job_dsl_app_script.groovy
@@ -3,6 +3,11 @@ folder("${appName}") {
         description("Descripción del Folder para proyecto ${appName}")
 	job("${appName}/job_${appName}_for_components") {
 		description('Job DSL para generar un nuevo Job')
+		parameters {
+    stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
+    choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+    booleanParam('agente', false)
+  }
 		// Define los pasos para clonar el repositorio privado utilizando las credenciales
 		scm {
 			git {
@@ -10,6 +15,7 @@ folder("${appName}") {
 					url('https://github.com/Roger2608/CICD.git')
 					credentials('JenkinsGithub')
 				}
+				branch('*/feature/jobDSLForAPP')
 			}
 		}
 	}
