e6f163506dd05ddf04512f4df1f33193e5969138
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2023-07-29 17:35:12 -0500

    esta es

--- devops/jenkins/component/job_dsl_component_script.groovy
+++ devops/jenkins/component/job_dsl_component_script.groovy
@@ -7,6 +7,8 @@ pipelineJob('hello-pipeline-inline') {
 						userRemoteConfig {
 							url('https://github.com/Roger2608/CICD.git')
 							credentialsId('JenkinsGithub')
+							name('origin')
+							refspec('')
 						}
 				}
 				branches {
