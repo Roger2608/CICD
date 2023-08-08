20484351336bd7782938e823ccd9864e1e1d80bc
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2023-08-08 11:59:11 -0500

    add requirements and format .groovy

--- devops/jenkins/app/job_dsl_app_script.groovy
+++ devops/jenkins/app/job_dsl_app_script.groovy
@@ -1,26 +1,26 @@
 folder("${appName}") {
-  		displayName("${appName}")
-        description("Descripción del Folder para proyecto ${appName}")
-	job("${appName}/job_${appName}_for_components") {
-		description('Job DSL para generar un nuevo Job')
-		parameters {
-    stringParam('name', defaultValue = 'write_name_of_component', description = 'name of component')
-stringParam('app', defaultValue = "${appName}", description = 'name of app')
-  }
-		// Define los pasos para clonar el repositorio privado utilizando las credenciales
-		scm {
-			git {
-				remote {
-					url('https://github.com/Roger2608/CICD.git')
-					credentials('JenkinsGithub')
-				}
-				branch('*/feature/jobDSLForAPP')
-			}
-		}
-		steps {
-    dsl {
-      external('devops/jenkins/component/job_dsl_component_script.groovy')
+  displayName("${appName}")
+  description("Descripción del Folder para proyecto ${appName}")
+  job("${appName}/job_${appName}_for_components") {
+    description('Job DSL para generar un nuevo Job')
+    parameters {
+      stringParam('name', defaultValue = 'write_name_of_component', description = 'name of component')
+      stringParam('app', defaultValue = "${appName}", description = 'name of app')
+    }
+    // Define los pasos para clonar el repositorio privado utilizando las credenciales
+    scm {
+      git {
+        remote {
+          url('https://github.com/Roger2608/CICD.git')
+          credentials('JenkinsGithub')
+        }
+        branch('*/feature/jobDSLForAPP')
+      }
+    }
+    steps {
+      dsl {
+        external('devops/jenkins/component/job_dsl_component_script.groovy')
+      }
     }
   }
-	}
 }
--- devops/jenkins/component/job_dsl_component_script.groovy
+++ devops/jenkins/component/job_dsl_component_script.groovy
@@ -1,5 +1,5 @@
 /*pipelineJob('hello-pipeline-inline') {
-  definition {    
+  definition {
   	cpsScmFlowDefinition{
   		scm {
 			gitSCM {
@@ -32,21 +32,21 @@
 */
 import java.util.UUID
 
-folder("${app}/my-components"){
-	displayName("my-components")
-        description("Descripción del Folder para componentes de ${app}")
+folder("${app}/my-components") {
+  displayName("my-components")
+  description("Descripción del Folder para componentes de ${app}")
 }
 multibranchPipelineJob("${app}/my-components/${name}") {
-	    branchSources {
-		git {
-			id(UUID.randomUUID().toString())
-			remote('https://github.com/Roger2608/CICD.git')
-			credentialsId('JenkinsGithub')
-		}
-	    }
-	    orphanedItemStrategy {
-			discardOldItems {
-				numToKeep(20)
-			}
-		}
-	}
+  branchSources {
+    git {
+      id(UUID.randomUUID().toString())
+      remote('https://github.com/Roger2608/CICD.git')
+      credentialsId('JenkinsGithub')
+    }
+  }
+  orphanedItemStrategy {
+    discardOldItems {
+      numToKeep(20)
+    }
+  }
+}
