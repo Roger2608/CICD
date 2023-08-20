d68566058baaf86a11fffac40412606d5102e9bd
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2023-08-20 00:14:21 -0500

    update path

--- api/expose/azure/resource_controller.py
+++ api/expose/azure/resource_controller.py
@@ -6,6 +6,6 @@ from service.azure import resource_group_service as rg_service
 resource_group_route = APIRouter()
 
 
-@resource_group_route.post("azure/create/resource_group")
+@resource_group_route.post("/azure/create/resource_group")
 def create_resource_group(new_resource: RequestCreateResourceGroup):
     return rg_service.create_resource_group(new_resource)
