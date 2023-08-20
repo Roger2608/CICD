e771f5c881d0750681ba199dda78ebf10cddac72
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2023-08-19 23:26:02 -0500

    add route azure in main app

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from api.expose.azure import resource_controller as azure
 from api.expose.bitbucket import repository_controller as repository, project_controller as project
 from api.expose.jenkins import jenkins_controller as jenkins
 
@@ -8,3 +9,4 @@ app = FastAPI()
 app.include_router(project.project_route)
 app.include_router(repository.repository_route)
 app.include_router(jenkins.jenkins_route)
+app.include_router(azure.resource_group_route)
